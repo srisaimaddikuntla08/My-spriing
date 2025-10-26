1e98ad436baf8ed88c1ece3c2d80a29b1b5bad5e
srisai810 <srisaimaddikuntlams@gmail.com>
2025-10-26 13:44:25 +0530

    created my first Bean

--- src/main/java/com/db/demo/DemoApplication.java
+++ src/main/java/com/db/demo/DemoApplication.java
@@ -12,7 +12,28 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
 
+		BeanController beanController = ctx.getBean(BeanController.class);
+		beanController.displayBean();
+
 	}
 
+	@Component
+	class MyBean{
+		public void display(){
+			System.out.println("created a firts Bean");
+		}
+	}
 
+	@Component
+	class BeanController{
+		private final MyBean myBean;
+
+		 BeanController(MyBean myBean){
+			this.myBean = myBean;
+		}
+
+		public void displayBean(){
+			myBean.display();
+		}
+	}
 }
