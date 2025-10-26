fa4e81bf643e2dcc9146e4acc2a1a412119d503c
srisai810 <srisaimaddikuntlams@gmail.com>
2025-10-26 13:39:33 +0530

    crated a firstBean

---
+++ src/main/java/com/db/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.db.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+
+	}
+
+
+}
