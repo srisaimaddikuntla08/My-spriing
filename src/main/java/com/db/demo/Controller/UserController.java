d28e020979297da683742bea2f5ea0127b82edbc
srisai810 <srisaimaddikuntlams@gmail.com>
2025-11-07 11:22:58 +0530

    setted service laye

--- src/main/java/com/db/demo/Controller/UserController.java
+++ src/main/java/com/db/demo/Controller/UserController.java
@@ -2,55 +2,26 @@ package com.db.demo.Controller;
 
 
 import com.db.demo.Entity.UserEntity;
-import org.apache.catalina.User;
-import org.springframework.http.HttpStatus;
+import com.db.demo.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
 public class UserController {
 
-    private final Map<Integer, UserEntity> userDb = new HashMap<>();
-
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/users")
-    public ResponseEntity<Map<Integer,UserEntity>> getUsers(){
-        return ResponseEntity.ok(userDb);
+    public ResponseEntity<Map<Integer, UserEntity>> getAllUsers(){
+        return ResponseEntity.ok(userService.getAllUsers());
     }
-
-
 
     @PostMapping("/user")
     public ResponseEntity<String> createUser(@RequestBody UserEntity userEntity){
-       if(userDb.containsKey(userEntity.getId())){
-           return ResponseEntity.status(HttpStatus.CONFLICT).body("user already existed");
-       }
-       userDb.put(userEntity.getId(),userEntity);
-       return ResponseEntity.status(HttpStatus.CREATED).body("created");
+        return ResponseEntity.ok(userService.createUser(userEntity));
     }
-
-
-    @PatchMapping("/user/{id}")
-    public ResponseEntity<String> updateUser(@PathVariable int id,@RequestBody UserEntity userEntity){
-        if(!userDb.containsKey(id)){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("user not there");
-        }
-        userDb.put(userEntity.getId(),userEntity);
-        return ResponseEntity.status(HttpStatus.CREATED).body("updated");
-    }
-
-
-    @DeleteMapping("/user/{id}")
-    public ResponseEntity<String> deleteUser(@PathVariable int id){
-        if(!userDb.containsKey(id)){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("user not there");
-        }
-        userDb.remove(id);
-        return ResponseEntity.ok("deleted");
-    }
-
 }
--- src/main/java/com/db/demo/Entity/UserEntity.java
+++ src/main/java/com/db/demo/Entity/UserEntity.java
@@ -1,17 +1,17 @@
 package com.db.demo.Entity;
 
+
 public class UserEntity {
+    int id ;
+    String email;
+    String name;
 
-    private int id;
-    private  String name;
-    private String email;
+    public UserEntity(){}
 
-
-    public UserEntity(){};
-    public UserEntity(int id,String name,String email){
+    public UserEntity(int id,String email,String name){
         this.id = id;
-        this.name = name;
         this.email = email;
+        this.name = name;
     }
 
 
@@ -23,19 +23,19 @@ public class UserEntity {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
